0d4f15ff37ad504cb51e7b6d6d65525f25997058
kpgwebdev <kpgwebdev@gmail.com>
2021-07-22 13:27:18 -0400

    added in quotes array

---
+++ js/script.js
@@ -0,0 +1,57 @@
+/************************************************
+Treehouse Techdegree:
+Project 4 - Random Quote Generator in JavaScript
+*************************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://uboraacademy.slack.com
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+    quote:"The way to get started is to quit talking and begin doing.",
+    source:"Walt Disney"
+   },
+  {
+    quote:"Nobody in life gets exactly what they thought they were going to get. But if you work really hard and you’re kind, amazing things will happen.",
+    source:"Conan O'Brien"
+  },
+  {
+    quote:"Fight for the things that you care about, but do it in a way that will lead others to join you.",
+    source:"Ruth Bader Ginsburg"
+  },
+  {
+    quote:"Believe you can and you're halfway there.",
+    source:"Theodore Roosevelt"
+  },
+  {
+    quote:"Sometimes you will never know the value of a moment, until it becomes a memory.",
+    source:"Dr. Seuss",
+    citation: "Seuss-isms!",
+    Year: 1966
+  } 
+];
+
+// console.log(quotes);
+
+/***
+ * `getRandomQuote` function
+***/
+function getRandomQuote() {}
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
